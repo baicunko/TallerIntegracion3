2305dd420e54b9ae5d6182d0e1ee37a86f5f7e32
Ubuntu <servidor@servidor.cl>
2014-04-08 22:29:01 -0300

    First Subida

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsApp::Application.config.secret_key_base = 'ee8525166420c60a79c04ba3887077fd007a92265c0aa135135dbcfa5cff79d5268df8408057f35859076f994ac5bdff0a553cc1cc1071e2ec27fb4bd3588c1a'
