128fb269ac3dcaee0f73c4890cc35e35c52cc2fc
Cristian Lehuede <cristianlehuede@gmail.comm>
2014-04-28 15:16:00 -0400

    Probando conexion

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'jquery-rails'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
-
+gem 'dropbox-sdk'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,21 @@
 module ApplicationHelper
-	def self.rafael
-		puts "SOY FEO"
-		RAILS_DEFAULT_LOGGER.error("\n test \n")
-		p=Spree::Product.find_by_id(4);
-	end
+  APP_KEY="ic0hidmnoxyvp9q"
+  APP_SECRET="n80ou9i5e4u9iix"
+  require 'dropbox_sdk'
+
+  def self.connect
+    code = "wsWFRrcC0rkAAAAAAAAAC925LJBoi7yUZFXn2NyLtuqLapTzJqvoPCav4Pm4sIys"
+    client = DropboxClient.new(code)
+    puts "linked account:", client.account_info().inspect
+    contents, metadata = client.get_file_and_metadata('/Grupo3/DBPrecios.accdb')
+
+    destination_file_full_path = Rails.root.to_s + "/" + "DBPrecios.accdb"
+
+
+      open(destination_file_full_path, 'wb') do |file|
+        file << open(contents).read
+      end
+    puts destination_file_full_path
+    open('DBPrecios.accdb', 'w') {|f| f.puts contents }
+  end
 end
