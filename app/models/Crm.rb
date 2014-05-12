798b340c38ba0fd9d63f5e77209ccedb3fb7cf4c
Natalia Garcia <ngarcia@uc.cl>
2014-05-11 23:08:09 -0400

    Avance
    login y retrieve del CRM

--- app/models/Crm.rb
+++ app/models/Crm.rb
@@ -5,17 +5,37 @@ class Crm < ActiveRecord::Base
 def self.getchallenge
 
     user_name = 'grupo3'
-    resp1 = RestClient.get 	'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=getchallenge',:username => user_name
-	token = JSON.parse(resp1.body)['result']['token']
+	vturl = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation='
+	gcurl = vturl + 'getchallenge&username=grupo3'
+    getc = RestClient.get 	gcurl
+	token = JSON.parse(getc.body)['result']['token']
 
-    urlogin = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=login'
-	
+    urlogin = vturl + 'login'	
     @md5 = Digest::MD5.hexdigest(token+'0GQ1snGfCFvIHpcT')
-    post = RestClient.post urlogin, {:params => {
+    log = RestClient.post urlogin, 
          'operation' => 'login',
          'username' => user_name,
-         'accessKey' => @md5 }}
-	resp = JSON.parse(post.body)['success']
-	return resp
+         'accessKey' => @md5
+	 
+	sessionId = JSON.parse(post.body)['result']['sessionName']
+	
+	urlisttypes =  vturl 'listtypes&sessionName='+sessionId
+	
+	lista = RestClient.get urlisttypes
+	deco = JSON.parse(lista.body)['result']['types']
+	
+	describe = RestClient.get vturl+'describe&sessionName='+sessionId+'&elementType=Accounts'	
+	idprefix = JSON.parse(describe.body)['result']['idPrefix']
+		
+	urlretrieve = vturl+'retrieve&sessionName='+sessionId+'&id='+idprefix+'x3167'	
+	ret = RestClient.get urlretrieve
+	
+	retr = JSON.parse(ret.body)['result']
+	
+	#extendurl = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=extendsession'	
+	#ext = RestClient.post extendurl
+	
+	
+	return retr
   end
 end
