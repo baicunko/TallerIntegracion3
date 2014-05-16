acabccc4d4c2837f7be9dfbe6ff398c5f0778b77
Cristian Lehuede <cristianlehuede@gmail.comm>
2014-05-16 15:33:34 -0400

    Listo

--- app/helpers/loading_helper.rb
+++ app/helpers/loading_helper.rb
@@ -2,8 +2,6 @@ module LoadingHelper
   require 'CSV'
   require 'date'
   def self.import
-
-
     file = File.open(File.join(Rails.root, 'Pricing.csv'), "r+")
     buffer = file.read
     file.rewind
@@ -15,28 +13,14 @@ module LoadingHelper
 
     file = File.join(Rails.root, 'Pricing.csv')
 	  File.read(file)
-
     CSV.foreach("#{Rails.root}/Pricing.csv", headers: true) do |row|
-
-
       product_hash = row.to_hash # exclude the price field
-
-
-
       arregloFechas=product_hash["fecha_actualizacion"].split('/')
       fechaArreglada=arregloFechas[1]+"/"+arregloFechas[0]+"/"+arregloFechas[2]
       product_hash["fecha_actualizacion"]=fechaArreglada
-
-
-
       probando=product_hash["fecha_vigencia"].split('/')
       fechaSuperArreglada=probando[1]+"/"+probando[0]+"/"+probando[2]
       product_hash["fecha_vigencia"]=fechaSuperArreglada
-
-
-
-
-
       product = Product.where(id: product_hash["id"])
 
       if product.count == 1
@@ -46,4 +30,26 @@ module LoadingHelper
       end # end if !product.nil?
     end # end CSV.foreach
   end # end self.import(file)
+
+
+  def self.importProductosJson
+
+
+    #Open file
+
+    file = File.join(Rails.root, 'Productos.csv')
+    File.read(file)
+    CSV.foreach("#{Rails.root}/Productos.csv", headers: true) do |row|
+      product_hash = row.to_hash # exclude the price field
+      p product_hash
+        ProductosJson.create!(product_hash)
+
+    end # end CSV.foreach
+  end # end self.import(file)
+
+
+
+
+
+
 end
