48cfb5c5e785885b8d674179fb41fc678ce621a2
Joaquin Zavala <joaquin.zavala@gmail.com>
2014-05-16 11:43:45 -0400

    Update import method

--- app/helpers/loading_helper.rb
+++ app/helpers/loading_helper.rb
@@ -9,12 +9,12 @@ module LoadingHelper
     CSV.foreach("#{Rails.root}/app/products.csv", headers: true) do |row|
 
       product_hash = row.to_hash # exclude the price field
-      product = ProductAccess.where(id: product_hash["id"])
+      product = Product.where(id: product_hash["id"])
 
       if product.count == 1
         product.first.update_attributes(product_hash.except("price")) # exclude the price field.
       else
-        ProductAccess.create!(product_hash)
+        Product.create!(product_hash)
       end # end if !product.nil?
     end # end CSV.foreach
   end # end self.import(file)
