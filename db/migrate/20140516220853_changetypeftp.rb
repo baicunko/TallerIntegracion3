9d3d2f09f63938b4b6204d71083aa5edcce0d2ea
Cristian Lehuede <cristianlehuede@gmail.comm>
2014-06-22 12:52:52 -0400

    Migrations mejoradas

--- db/migrate/20140516220853_changetypeftp.rb
+++ db/migrate/20140516220853_changetypeftp.rb
@@ -1,20 +1,19 @@
 class Changetypeftp < ActiveRecord::Migration
   def change
-  	#change_column :ftp_pedidos, :sku, 'integer USING CAST(sku AS integer)' migracion PostgreSQL
+  	change_column :ftp_pedidos, :sku, 'integer USING CAST(sku AS integer)'
+  	remove_column :ftp_pedidos, :fecha
+  	add_column :ftp_pedidos, :fecha, :datetime
+  	remove_column :ftp_pedidos, :hora
+  	add_column :ftp_pedidos, :hora, :datetime
+  	remove_column :ftp_pedidos, :entrega
+  	add_column :ftp_pedidos, :entrega, :datetime
+  	add_column :ftp_pedidos, :id, :string
 
-  	#remove_column :ftp_pedidos, :fecha
-  	#add_column :ftp_pedidos, :fecha, :datetime
-  	#remove_column :ftp_pedidos, :hora
-  	#add_column :ftp_pedidos, :hora, :datetime
-  	#remove_column :ftp_pedidos, :entrega
-  	#add_column :ftp_pedidos, :entrega, :datetime
-  	#add_column :ftp_pedidos, :id, :string
 
-
-    change_column :ftp_pedidos, :sku, :integer
-    change_column :ftp_pedidos, :fecha, :datetime
-    change_column :ftp_pedidos, :hora, :datetime
-    change_column :ftp_pedidos, :entrega, :datetime
-    add_column("ftp_pedidos","id",:string)
+    #change_column :ftp_pedidos, :sku, :integer
+    #change_column :ftp_pedidos, :fecha, :datetime
+    #change_column :ftp_pedidos, :hora, :datetime
+    #change_column :ftp_pedidos, :entrega, :datetime
+    #add_column("ftp_pedidos","id",:string)
   end
 end
