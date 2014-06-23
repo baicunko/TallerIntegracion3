5942f317ac2ba447edf3fba59dc70914c23162a3
Ubuntu <servidor@servidor.cl>
2014-06-22 21:11:16 -0400

    DSADAS

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140622225526) do
+ActiveRecord::Schema.define(version: 20140622234156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
